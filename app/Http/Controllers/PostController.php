5242c0f4ef2bd18edfc0bfda237dd73f121d8a14
Domeshow <domeshowemmanuel@gmail.com>
2022-04-08 15:07:03 +0200

    [ENH] add relationship for Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,20 @@ class PostController extends Controller
 {
     //
     public function index() {
+        $posts = Post::with(['user','category'])->get();
+        $posts->makeHidden(['user_id', 'category_id']);
         return response()->json([
                 "status"=> 200,
-                "posts"=>Post::get()
+                "posts"=>$posts
             ], 200);
+    }
+
+    public function show($id) {
+        $post = Post::whereId($id)->with(['user','category'])->get();
+        $post->makeHidden(['user_id', 'category_id']);
+        return response()->json([
+            "status"=> 200,
+            "post"=> $post
+        ]);
     }
 }
